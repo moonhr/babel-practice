47c6109622b9532437b2cef039d0d2cf4facd691
moonhr <moonhr153@gmail.com>
2024-07-03 15:47:03 +0900

    폴더 위치 변경

--- app.babel.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-function a() {
-  var result = (arguments.length <= 0 ? undefined : arguments[0]) + (arguments.length <= 1 ? undefined : arguments[1]);
-  var onemore = function onemore() {
-    return result + 10;
-  };
-  return onemore();
-}
